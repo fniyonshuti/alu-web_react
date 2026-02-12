f0ea7f39180a51614d3689cc0947fbc939cc8072
fniyonshuti <f.niyonshut1@alustudent.com>
2026-02-12 22:27:39 +0200

    file deleted

--- Webpack/task_0/README.md
+++
@@ -1,8 +0,0 @@
-Basic setup
-
-This task sets up a minimal Webpack project with jQuery and a single entry point.
-
-Build steps:
-
-1. Install dependencies: `npm install`
-2. Build bundle: `npm run build`
--- Webpack/task_0/src/index.js
+++
@@ -1,9 +0,0 @@
-// Import jQuery
-import $ from 'jquery';
-
-// Add three paragraphs to the body
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
