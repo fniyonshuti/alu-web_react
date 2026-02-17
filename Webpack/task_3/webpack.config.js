e525dc8f1e08205641ddec13cac321381d0c626f
fniyonshuti <f.niyonshut1@alustudent.com>
2026-02-17 22:51:47 +0200

    Dev servers, modules, and tree shaking

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,66 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    header: "./modules/header/header.js",
-    body: "./modules/body/body.js",
-    footer: "./modules/footer/footer.js",
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
-  devtool: "inline-source-map",
   devServer: {
-    static: path.join(__dirname, "public"),
-    compress: true,
+    static: './public',
     port: 8564,
+    open: true,
   },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
+  devtool: 'inline-source-map',
+  performance: {
+    hints: false,
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        use: [
-          {
-            loader: "image-webpack-loader",
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65,
-              },
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-            },
-          },
-        ],
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Holberton Dashboard",
+      title: 'Holberton Dashboard',
     }),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 };
